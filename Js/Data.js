dae573050ec7be065601b4d894615d688aae1044
Abdall Sakr <abdallaskar2000@gmail.com>
2025-05-18 22:12:45 +0300

    edit name of photos  and some styling

--- Js/Data.js
+++ Js/Data.js
@@ -1,10 +1,10 @@
 export const projectsData = [
-    ["Drive easy", "A responsive car rental platform with an admin dashboard. Built with Bootstrap 5 and JavaScript.",
+    ["Drive easy", "A responsive car rental platform with an admin dashboard.",
         "https://abdallaskar.github.io/DriveEasy-Cars/", "https://github.com/abdallaskar/DriveEasy-Cars", "drive/1.jpg", "drive/2.jpg", "drive/3.jpg", "drive/4.jpg"],
     ["Weather App", "A weather forecasting application that provides real-time weather updates.",
         "https://abdallaskar.github.io/Weather_today/", "https://github.com/abdallaskar/Weather_today", "weather/1.jpg", "weather/2.jpg", "weather/3.jpg", "weather/4.jpg"],
     ["Sakr portfolio", "A personal portfolio website showcasing my projects and skills.",
-        "https://example.com", "https://github.com/yourusername/project2", "sakr/1.jpg", "sakr/2.jpg", "sakr/3.jpg", "sakr/4.jpg"],
+        "https://abdallaskar.github.io/Sakr_portfolio/", "https://github.com/abdallaskar/Sakr_portfolio", "sakr/1.jpg", "sakr/2.jpg", "sakr/3.jpg", "sakr/4.jpg"],
     ["ToDo organizer", "A task management application to help users organize their tasks efficiently.",
         "https://abdallaskar.github.io/Todo_list_app/", "https://github.com/abdallaskar/Todo_list_app", "todo/1.jpg", "todo/2.jpg", "todo/3.jpg", "todo/4.jpg"],
     ["E-Recipt", "A recipe application that provides users with a variety of recipes and cooking tips.",
@@ -37,7 +37,22 @@ export const longDescription = [
         🌄 Dynamic backgrounds that adapt to weather conditions.<br>
         📱 Responsive design with a glassmorphism aesthetic.<br>
 `],
-    [],
+    [` <h3> Abdalla Sakr – Portfolio Website </h3>
+        Welcome to my personal portfolio website! This site showcases my web development projects, my technical and soft skills,
+        and how to contact me for collaboration or freelance opportunities.
+        <h4>📌 Sections Overview</h4>
+        🏠 Home <br>
+        A responsive landing section introducing me as a web developer.<br>
+        👨‍💻 About Me <br>
+        A brief introduction about who I am and my background.<br>
+        🧠 Skills <br>
+        A dynamic section highlighting both technical and soft skills. <br>
+        📂 Projects <br>
+        A summary of the projects I’ve included in the Projects section <br>
+        📞 Contact Me <br>
+        All the ways to contact me, including my social media links.<br>
+        `],
+
     [`<h3>To-Do List Application</h3>
         A clean, intuitive task manager with persistent storage<br>
 
